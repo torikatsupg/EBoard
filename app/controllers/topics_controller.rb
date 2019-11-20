4ddfdf8219d3a521a00fa14b5aae089aa1d3ba56
鳥居克哉 <toriikatsuya@toriikatsuyanoMacBook-Pro.local>
2019-11-21 01:02:34 +0900

    TopicsのCRUD作成

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,2 +1,33 @@
 class TopicsController < ApplicationController
+  def index
+    @new_topic = Topic.new
+    @topics = Topic.all
+  end
+
+  def create
+    Topic.create(topic_params)
+    redirect_to root_url
+  end
+
+  def destroy
+    Topic.find(params[:id]).destroy
+    redirect_to root_url
+  end
+
+  def edit
+    @topic = Topic.find(params[:id])
+  end
+
+  def update
+    @topic = Topic.find(params[:id])
+    @topic.title = params[:topic][:title]
+    @topic.save
+    redirect_to root_url
+  end
+
+  private
+
+  def topic_params
+    params.require(:topic).permit(:title)
+  end
 end
